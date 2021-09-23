ccc4e3534f75784ce0f121b572de00ec6ccfd199
Ruben Mavarez <rubenmavarezb@gmail.com>
2021-09-23 09:43:24 -0300

    ajax

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
-const products = [
-    {
-        id:'1',
-        nombre:'producto1',
-        precio: 1000,
-        stock:15,
-        cantidad: 0
-    },
-    {
-        id:'2',
-        nombre:'producto2',
-        precio: 2000,
-        stock: 20,
-        cantidad: 0
-    },
-    {
-        id:'3',
-        nombre:'producto3',
-        precio: 3000,
-        stock: 30,
-        cantidad: 0
-    },
-];
+console.log('segundo console.log')
+let products = [];
 
 let carrito = [];
 
+const addToCart = (evento, id) => {
+    console.log('Products que ya estan cargados',products);
+    const cantidades = evento.target.previousElementSibling.value;
+    //console.log(cantidades)
+    //Buscamos si existe en el carrito
+    const prodIsInCart = isInCart(id);
+    //console.log(prodIsInCart);
+    
+    //Falsey values
+    //undefined, null, 0, '', unknown, false
+    //Truthy values 
+    //numeros > 0, strings que no sean vacios, true, {}, []
+
+    if(prodIsInCart) {
+        //Buscamos el producto en el carrito para modificarlo
+        const indexDelProd = carrito.findIndex(prod => prod.id === prodIsInCart.id);
+        const product = carrito[indexDelProd]
+        if((product.cantidad < prodIsInCart.stock ) && (parseInt(product.cantidad) + parseInt(cantidades) < prodIsInCart.stock )) {
+            product.cantidad = parseInt(product.cantidad) + parseInt(cantidades);
+        } else {
+            alert('No podes agregar mas master!');
+            product.cantidad = prodIsInCart.stock;
+        }
+        renderProductsInCart(carrito, '.cart');
+        return;
+    }
+
+    const productoEnBaseDeDatos = products.find(producto => producto.id === id);
+    //console.log(productoEnBaseDeDatos);
+    productoEnBaseDeDatos.cantidad = cantidades;
+    //console.log(productoEnBaseDeDatos)
+    carrito.push(productoEnBaseDeDatos);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    renderProductsInCart(carrito,'.cart');
+}
+
 const renderProducts = (arrayDeProductos, elemento) => {
     const productDiv = document.querySelector(elemento);
+
+    if(arrayDeProductos.length === 0) {
+        productDiv.innerHTML = 'No hay productos en la tineda :(((';
+        return;
+    }
     //console.log(productDiv)
     productDiv.innerHTML = '';
     let html = '';
@@ -36,7 +56,7 @@ const renderProducts = (arrayDeProductos, elemento) => {
                 <p>${articulo.nombre}</p>
                 <span>${articulo.precio}$</span>
                 <input class="input-qty input-qty-${articulo.id}" type="number" value="1">
-                <button class="btn-comprar" value=${articulo.id}>Comprar</button>
+                <button class="btn-comprar" onclick="addToCart(event,'${articulo.id}')">Comprar</button>
             </article>
         `;
     })
@@ -78,54 +98,36 @@ const renderProductsInCart = (arrayDeProductos, elemento) => {
 
 const isInCart = (productId) => carrito.find(prod => prod.id === productId);
 
-const addToCart = (evento) => {
-    const idDelProducto = evento.target.value;
-    const cantidades = evento.target.previousElementSibling.value;
-    console.log(cantidades)
-    //Buscamos si existe en el carrito
-    const prodIsInCart = isInCart(idDelProducto);
-    //console.log(prodIsInCart);
-    
-    //Falsey values
-    //undefined, null, 0, '', unknown, false
-    //Truthy values 
-    //numeros > 0, strings que no sean vacios, true, {}, []
-
-    if(prodIsInCart) {
-        //Buscamos el producto en el carrito para modificarlo
-        const indexDelProd = carrito.findIndex(prod => prod.id === prodIsInCart.id);
-        const product = carrito[indexDelProd]
-        if((product.cantidad < prodIsInCart.stock ) && (parseInt(product.cantidad) + parseInt(cantidades) < prodIsInCart.stock )) {
-            product.cantidad = parseInt(product.cantidad) + parseInt(cantidades);
-        } else {
-            alert('No podes agregar mas master!');
-            product.cantidad = prodIsInCart.stock;
-        }
-        renderProductsInCart(carrito, '.cart');
-        return;
-    }
-
-    const productoEnBaseDeDatos = products.find(producto => producto.id === idDelProducto);
-    //console.log(productoEnBaseDeDatos);
-    productoEnBaseDeDatos.cantidad = cantidades;
-    //console.log(productoEnBaseDeDatos)
-    carrito.push(productoEnBaseDeDatos);
-    localStorage.setItem('carrito', JSON.stringify(carrito));
-    renderProductsInCart(carrito,'.cart');
-}
-
-
 window.onload = () => {
-    renderProducts(products, '.products');
+
+    console.log('Primer console log')
+
     if(carrito.length === 0) {
         document.querySelector('.cart').innerHTML = '<h2>No hay productos :(</h2>'
     }
 
-    const btns = document.querySelectorAll('.btn-comprar');
+    $.get("/products.json", function( data ) {
+        console.log('Data:',data['1'])
+        products = [...data['0']];
+        // console.log('Products despues de ejecutar el codigo: ', products);
+        renderProducts(products, '.products'); 
+    });
+    
+    //renderProducts(products, '.products');
+    //No hace falta porque el asincronismo cambio la forma en la que se ejecutaba el codigo
+    //const btns = document.querySelectorAll('.btn-comprar');
     //console.log(inputQty)
     //console.log(btns);
-    btns.forEach(button => button.addEventListener('click', addToCart))
+    //btns.forEach(button => button.addEventListener('click', addToCart))
 }
+
+
+
+
+
+
+
+
 
 //OFF TOPIC 
 
